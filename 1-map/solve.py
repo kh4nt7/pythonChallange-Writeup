0d933187830805272cdddd2d41e97aef2c933593
kh4nt99 <kmkhant7777@gmail.com>
2019-04-24 22:55:56 +0630

    Update solve.py

--- 1-map/solve.py
+++ 1-map/solve.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python
+string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+text = ""
+diff = 0
+
+for c in string:
+	char = chr(ord(c)+2)
+	if char=="{": char="a"
+	if c==" ": char=c
+	if c==".": char=c
+	if c=="z": char="b"
+	text += char
+
+print text
